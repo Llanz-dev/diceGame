66a71ea738ff70b6cb2a4b987ab902ecc95eca76
llanz-dev <llanzkennethluy@gmail.com>
2020-10-05 00:57:56 +0800

    commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function capitalizeFirstLetter(string) {
+    let arr = string.split(' ');
+    let cap = arr.map(i => {
+       return i[0].toUpperCase() + i.slice(1).toLowerCase()
+    });
+    return cap.join(' ');
+    }
+    var firstPlayer = document.getElementById("playerOne").innerHTML = capitalizeFirstLetter(localStorage.getItem("player1", firstPlayer));
+    var secondPlayer = document.getElementById("playerTwo").innerHTML = capitalizeFirstLetter(localStorage.getItem("player2", secondPlayer));
+    firstPlayer = capitalizeFirstLetter(firstPlayer);
+    secondPlayer = capitalizeFirstLetter(secondPlayer);
     function restart(){
         location.reload(true);
     }
@@ -5,18 +16,15 @@
         var number1 = Math.random() * 6 + 1;
         var numberRound1 = Math.floor(number1);
         var displayResult1 = "images/dice" + numberRound1 + ".png";
-        
         var number2 = Math.random() * 6 + 1;
         var numberRound2 = Math.floor(number2);
         var displayResult2 = "images/dice" + numberRound2 + ".png";
-
         document.getElementById("img1").src = displayResult1;
         document.getElementById("img2").src = displayResult2;
-
         if(numberRound1 > numberRound2){
-            document.querySelector("h1").innerHTML = "Player 1 Wins!"
+            document.querySelector("h1").innerHTML = firstPlayer + " Wins!"
         } else if (numberRound2 > numberRound1){
-            document.querySelector("h1").innerHTML = "Player 2 Wins!"
+            document.querySelector("h1").innerHTML = secondPlayer + " Wins!"
         } else {
             document.querySelector("h1").innerHTML = "Draw!"
         }   
